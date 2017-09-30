783bcbdf176992eb218883e2e96020a3ffb48bd9
steryn <765500814@qq.com>
2017-09-30 15:44:59 +0800

    gulp环境搭建完成,整合sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp'); //requires the core Gulp library
 var g = require('gulp-load-plugins')(); //read the dependencies in the package.json file and inject each of them for us.
 var browserSync = require('browser-sync');
-var del = require('del');
 var runSequence = require('run-sequence');
+var sass = require('gulp-ruby-sass');
+var del = require('del');
 
 //jshint block
 //Stylish reporter for JSHint(reporter looked more beauty)
@@ -25,47 +26,55 @@ var paths = {
         less: 'app/style/**/*.less',
         js: 'app/js/**/*.js',
         img: 'app/img/**/*',
-        html: 'app/views/**/*.html'
+        html: 'app/views/**/*.html',
+        sass: 'app/style/**/*.scss',
     }
 };
 
 // Clean dist
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     return del('dist/*');
 });
 //打包合并js
-gulp.task('scripts',function(){
+gulp.task('scripts', function () {
     gulp.src(['./src/js/*.js'])
-    .pipe(concat('all.js'))
-    .pipe(srtipDebug())
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
+        .pipe(concat('all.js'))
+        .pipe(srtipDebug())
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/js'));
 })
 
 //minifies js
-gulp.task('minifyJS', function() {
+gulp.task('minifyJS', function () {
     return gulp.src(paths.app.js)
         .pipe(g.uglify())
         .pipe(gulp.dest(paths.dist.js));
 });
 
 // Inspect js
-gulp.task('jshint', function() {
+gulp.task('jshint', function () {
     gulp.src(paths.app.js)
         .pipe(g.jshint(packageJSON.jshintConfig))
         .pipe(g.jshint.reporter(stylish))
 });
 
 //compress less & minify css
-gulp.task('less', function() {
+gulp.task('less', function () {
     return gulp.src(paths.app.less)
         .pipe(g.less())
         .pipe(g.minifyCss())
         .pipe(gulp.dest(paths.dist.css));
 });
 
+// style有以下4种选择：nested：嵌套缩进，它是默认值expanded：每个属性占一行,compact：每条样式占一行,compressed：整个压缩成一行
+gulp.task('sass', function () {
+    sass(paths.app.sass, { style: 'compact' })
+        .on('error', sass.logError)
+        .pipe(gulp.dest(paths.dist.css))
+});
+
 //minify HTML
-gulp.task('minifyHTML', function() {
+gulp.task('minifyHTML', function () {
     return gulp.src(paths.app.html)
         .pipe(g.htmlmin({
             collapseWhitespace: true
@@ -74,14 +83,15 @@ gulp.task('minifyHTML', function() {
 });
 
 // Watching static resources
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch(paths.app.html, ['minifyHTML']);
     gulp.watch(paths.app.less, ['less']);
     gulp.watch(paths.app.js, ['jshint', 'minifyJS']);
+    gulp.watch(paths.app.sass, ['sass']);
 });
 
 // Synchronously update browser
-gulp.task('browser', function() {
+gulp.task('browser', function () {
     browserSync({
         //proxy: 'localhost:3000',
         //port: 3001,
@@ -93,16 +103,16 @@ gulp.task('browser', function() {
 });
 
 // Only build
-gulp.task('build', function() {
-    runSequence('clean', ['jshint', 'minifyJS', 'less', 'minifyHTML']);
+gulp.task('build', function () {
+    runSequence('clean', ['jshint', 'minifyJS', 'sass', 'minifyHTML']);
 });
 
 // Build & Watch
-gulp.task('build-watch', ['build', 'watch'], function() {
-    console.log('App frontend is built and watching on static resources...');
+gulp.task('build-watch', ['build', 'watch'], function () {
+    // console.log('App frontend is built and watching on static resources...');
 });
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     //Run a series of dependent gulp tasks in order
     runSequence('build-watch', 'browser');
 });
